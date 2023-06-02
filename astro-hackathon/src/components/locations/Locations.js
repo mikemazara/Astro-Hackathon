5d4cc3a273611b463f6880ead375e150548d2e9c
Alina <allianpi@gmail.com>
2023-06-02 13:14:28 -0400

    mooore styling

--- astro-hackathon/src/components/locations/Locations.js
+++ astro-hackathon/src/components/locations/Locations.js
@@ -3,7 +3,7 @@ const Locations = () => {
   return (
     <div className="locations">
       <div className="locations__container locations__container--ISS">
-        <h2>Locations</h2>
+        <h2 className="locations__title">Locations</h2>
         <div className="location__info location__info--ISS">
           <article className="location__card">
             <h3 className="location__name">Location Name</h3>
@@ -13,13 +13,13 @@ const Locations = () => {
         </div>
       </div>
       <div className="locations__container locations__container--earth">
-        <h2>Locations</h2>
+        <h2 className="locations__title">Locations</h2>
         <div className="location__info location__info--earth">
           <article className="location__card">
             <h3 className="location__name">Location Name</h3>
             <h3 className="location__name">Location Name</h3>
             <h3 className="location__name">Location Name</h3>
-            <button>open the map</button>
+            <button className="locations__btn">open the map</button>
           </article>
         </div>
       </div>
