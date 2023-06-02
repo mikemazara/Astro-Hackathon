e4a92d538f15334890ff28cb4c49bca6558fb6b3
altepapi <kojo@altepapis-mbp.home>
2023-06-02 13:11:50 -0400

    add map component, defaul coordinate, san fran

--- astro-hackathon/src/components/view/View.js
+++ astro-hackathon/src/components/view/View.js
@@ -1,9 +1,30 @@
-const Views = () => {
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
+import { useMemo, useState } from "react";
+import "./view.scss";
+
+const MapContainer = () => {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+  });
+
+  
+  const center = useMemo(() => ({ lat: 37.7749, lng: -122.4194 }), []);
+
   return (
-    <div>
-      <div></div>
+    <div className="MapContainer">
+      {!isLoaded ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <GoogleMap
+            mapContainerClassName="map-container"
+            center={center}
+            zoom={11}
+          />
+        </div>
+      )}
     </div>
   );
 };
 
-export default Views;
+export default MapContainer;
